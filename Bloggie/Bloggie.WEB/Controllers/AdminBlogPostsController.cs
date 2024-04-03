a275fbaa9cee8d22c4b6405a0cf8e1c9b498e363
Emanuel <emanuel.fernandesdasilva@gmail.com>
2024-04-03 17:58:23 -0300

    Add New Blog post Admin funcionality
    Add New Blog post Admin funcionality

---
+++ Bloggie/Bloggie.WEB/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.WEB.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+           return View();
+        }
+    }
+}
